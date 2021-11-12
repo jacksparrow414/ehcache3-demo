d335eccfae5e43009a11f8f827ae771c42708567
Serendipity <jacksparrow414@163.com>
2021-11-12 16:45:52 +0800

    add comment

--- src/main/java/com/example/ehcache/EhcacheApplication.java
+++ src/main/java/com/example/ehcache/EhcacheApplication.java
@@ -3,6 +3,9 @@ package com.example.ehcache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * ehcache3 官方例子 https://github.com/ehcache/ehcache3-samples
+ */
 @SpringBootApplication
 public class EhcacheApplication {
 
